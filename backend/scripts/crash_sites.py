bc449fbf659dfacbe174be84b2b5eae5361bf1f3
Ben Stickelbrucks <ben.stickelbrucks@web.de>
2021-10-29 22:19:27 +0200

    zwischenstand

---
+++ backend/scripts/crash_sites.py
@@ -0,0 +1,23 @@
+import csv
+import json
+
+filenames = ['../data/Unfallorte.csv', '../data/Unfallorte2016.csv','../data/Unfallorte2017.csv','../data/Unfallorte2019.csv']
+
+class excel_semicolon(csv.excel):
+    delimiter = ';'
+
+
+data = []
+
+for f in filenames:
+    with open(f, 'r') as csvfile:
+        csv.register_dialect("excel-semicolon", excel_semicolon)
+        datareader = csv.DictReader(csvfile, dialect=excel_semicolon)
+        for row in datareader:
+           if row['ULAND'] == '09' and row['UREGBEZ'] == '1' and row['UKREIS'] == '62':
+                row['OBJECTID'] = row['OBJECTID'] + row['UJAHR']
+                data.append(row)
+
+with open('../data/Unfallorte.json', 'w', encoding='utf-8') as jsonf:
+    jsonf.write(json.dumps(data, indent=4))
+
