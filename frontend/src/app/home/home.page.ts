4ef21384ce51c0a321aa0fdf77244186cbc58e54
NewLordVile <roman.mayr@gmx.net>
2021-10-30 15:00:46 +0200

    wip: layer toggling

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import wifiObjects from '../../data/wifi_objects.json';
-import accessbilityObjects from '../../data/accessibility.json';
+import accessibility from '../../data/accessibility.json';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +11,9 @@ export class HomePage implements OnInit, OnDestroy{
   // google maps zoom level
   zoom = 11;
   hotspots = wifiObjects;
-  accessbility = accessbilityObjects
-  colorAccessibility = "#219653";
-  radiusAccessibility = "50";
+  accessibility = accessibility;
+  colorAccessibility = '#219653';
+  radiusAccessibility = '50';
   rectangleSet = [];
   // private heatmapAcc: google.maps.visualization.HeatmapLayer = null;
   // private map;
@@ -23,37 +23,42 @@ export class HomePage implements OnInit, OnDestroy{
   lat = 48.1351;
   lng = 11.5820;
 
+  wifiLayerActive = false;
+  safetyLayerActive = true;
+  accessibilityLayerActive = false;
+
   constructor() {
 
   }
+
   ngOnDestroy(): void {
 
   }
   ngOnInit(): void {
-    for (let i = 0; i < accessbilityObjects.length -1; ++i) {
-      for (let k = 0; k < accessbilityObjects[i].length -1; ++k) {
-        const upperLeft = accessbilityObjects[i][k];
-        const upperRight = accessbilityObjects[i][k];
-        const lowerLeft = accessbilityObjects[i+1][k];
-        const lowerRight = accessbilityObjects[i][k];
-        let intensityAVG = (upperLeft.intensity + upperRight.intensity + lowerLeft.intensity + lowerRight.intensity) / 4;
-        let newRectangle = {
-          strokeColor: "#FF0000",
-          strokeOpacity: 0.8,
-          strokeWeight: 2,
-          fillColor: "#FF0000",
-          fillOpacity: intensityAVG,
-          bounds: {
-            north: upperLeft.location[0],
-            south: lowerLeft.location[0],
-            east: lowerRight.location[1],
-            west: lowerLeft.location[1]
-          },
-        }
-        console.log(newRectangle)
-        this.rectangleSet.push(newRectangle);
-      }
-    }
+    // for (let i = 0; i < this.accessibility.length -1; ++i) {
+    //   for (let k = 0; k < this.accessibility[i].length -1; ++k) {
+    //     const upperLeft = this.accessibility[i][k];
+    //     const upperRight = this.accessibility[i][k];
+    //     const lowerLeft = this.accessibility[i+1][k];
+    //     const lowerRight = this.accessibility[i][k];
+    //     const intensityAVG = (upperLeft.intensity + upperRight.intensity + lowerLeft.intensity + lowerRight.intensity) / 4;
+    //     const newRectangle = {
+    //       strokeColor: '#FF0000',
+    //       strokeOpacity: 0.8,
+    //       strokeWeight: 2,
+    //       fillColor: '#FF0000',
+    //       fillOpacity: intensityAVG,
+    //       bounds: {
+    //         north: upperLeft.location[0],
+    //         south: lowerLeft.location[0],
+    //         east: lowerRight.location[1],
+    //         west: lowerLeft.location[1]
+    //       },
+    //     };
+    //     console.log(newRectangle);
+    //     this.rectangleSet.push(newRectangle);
+    //   }
+    // }
   }
 
   // public onMapLoad(mapInstance: google.maps.Map) {
@@ -74,4 +79,23 @@ export class HomePage implements OnInit, OnDestroy{
   //   });
   // }
 
+  get dataLoaded() {
+    return this.hotspots != null && this.accessibility != null;
+  }
+
+  toggleLayer(layer: string) {
+    if (layer === 'wifi') {
+      this.wifiLayerActive = !this.wifiLayerActive;
+      this.safetyLayerActive = false;
+      this.accessibilityLayerActive = false;
+    } else if (layer === 'safety') {
+      this.wifiLayerActive = false;
+      this.safetyLayerActive = !this.safetyLayerActive;
+      this.accessibilityLayerActive = false;
+    } else if (layer === 'accessibility') {
+      this.wifiLayerActive = false;
+      this.safetyLayerActive = false;
+      this.accessibilityLayerActive = !this.accessibilityLayerActive;
+    }
+  }
 }
