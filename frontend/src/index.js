d7db59e3d2115849b7d4769a7e8d339753b1eef7
tompa <tom.papke@outlook.de>
2021-10-30 20:25:31 +0200

    add polyline check

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,9 @@
 console.log(accMatrix)
 
+
 let map = new google.maps.Map(
     document.getElementById("map"), {
-        center: {lng: 11.613991700121016, lat:  48.2296360767711},
+        center: {lng: 11.613991700121016, lat: 48.2296360767711},
         zoom: 8,
     }
 );
@@ -11,17 +12,17 @@ function pickHex(weight) {
     weight = 1 - weight;
     if (weight <= 0.5) {
         weight *= 2;
-        return [255 , 255 *  weight, 0]
+        return [255, 255 * weight, 0]
     } else {
         weight -= 0.5;
         weight *= 2;
-        return [255 * (1 - weight), 255,0]
+        return [255 * (1 - weight), 255, 0]
     }
 }
 
-const RED = [255,0,0];
-const YELLOW = [255,255,0];
-const GREEN = [0,255,0];
+const RED = [255, 0, 0];
+const YELLOW = [255, 255, 0];
+const GREEN = [0, 255, 0];
 for (let i = 0; i < accMatrix.length - 1; i++) {
     for (let j = 1; j < accMatrix.length; j++) {
         let ne = accMatrix[i][j];
@@ -31,7 +32,7 @@ for (let i = 0; i < accMatrix.length - 1; i++) {
         const ne_loc = ne.location;
         const sw_loc = sw.location;
         const avgIntensity = (nw.intensity + ne.intensity + se.intensity + sw.intensity) / 4;
-        const color = pickHex( avgIntensity)
+        const color = pickHex(avgIntensity)
         const rectangleOptions = {
             bounds: new google.maps.LatLngBounds(
                 new google.maps.LatLng(sw_loc[0], sw_loc[1]),
@@ -45,3 +46,18 @@ for (let i = 0; i < accMatrix.length - 1; i++) {
         const newRectangle = new google.maps.Rectangle(rectangleOptions);
     }
 }
+
+const directionsService = new google.maps.DirectionsService()
+const directionsRenderer = new google.maps.DirectionsRenderer()
+directionsRenderer.setMap(map);
+directionsService.route({
+    origin: 'Marienplatz München',
+    destination: 'Fröttmanning München',
+    travelMode: google.maps.TravelMode.DRIVING,
+}, (result) => {
+    const path = google.maps.geometry.encoding.decodePath(result.routes[0].overview_polyline)
+    const polyline = new google.maps.Polyline({path: path});
+    console.log(google.maps.geometry.poly.isLocationOnEdge(
+        new google.maps.LatLng(48.13559829862524, 11.58075939171983), polyline, 10e-1))
+    directionsRenderer.setDirections(result);
+})
